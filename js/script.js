ea7886a018e382fb329861e2500d1de4fb863afd
KTerka <katarzynapawela@gmail.com>
2017-07-20 11:36:30 +0200

    Clock and contact

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+function initCarousel () {
 	var carouselList = $("#carousel ul");
 	
 	function moveFirstSlide(){
@@ -16,5 +16,33 @@ $(function(){
 	setInterval(changeSlide, 2000);
 	
 	$('.slogan').delay(500).fadeIn(2000);
+}
+
+function DisplayTime(){
+	var today = new Date();
+	
+	var day = today.getDate();
+	var month = today.getMonth()+1;
+	if (month<10) month = "0"+month;
+	var year = today.getFullYear();
+	
+	var hour = today.getHours();
+	if (hour<10) hour = "0"+hour;
+	var minute = today.getMinutes();
+	if (minute<10) minute = "0"+minute;
+	var seconds = today.getSeconds();
+	if (seconds<10) seconds = "0"+seconds;
+	
+	document.getElementById("clock").innerHTML = day+"/"+month+"/"+year+" | "+hour+":"+minute+":"+seconds;
+	setTimeout("DisplayTime()", 1000);
+}
+
+$(function(){
+
+	initCarousel();
+	
+	DisplayTime();	
+	
+	
 });
 
